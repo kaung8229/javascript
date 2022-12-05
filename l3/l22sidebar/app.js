1dd0fd86fc41bd2418c2713aef4984ca1e7be41b
Kaung Myat <kaung8229@gmail.com>
2022-12-05 08:31:01 +0630

    5-12-2022

---
+++ l3/l22sidebar/app.js
@@ -0,0 +1,14 @@
+// Get UI
+const openbtn = document.querySelector('.open-btn');
+const closebtn = document.querySelector('.close-btn');
+const getnav = document.querySelector('.nav');
+
+openbtn.addEventListener('click',function(){
+    getnav.classList.add('visible');
+})
+
+closebtn.addEventListener('click',function(){
+    getnav.classList.remove('visible');
+})
+
+
