bf70db2e02089a09c631b0f20385f5058043ba46
Kaung Myat <kaung8229@gmail.com>
2022-12-21 09:34:39 +0630

    21-12-2022

--- l3/l24videoplayer/app.js
+++ l3/l24videoplayer/app.js
@@ -7,7 +7,14 @@ const nextbtn = document.getElementById('next');
 const stopbtn = document.getElementById('stop');
 
 const progress = document.getElementById('progress');
+const getdisplaytime = document.getElementById('display-time');
 const getfullscreen = document.getElementById('fullscreen');
+
+const getcontainer = document.querySelector('.container');
+
+const getopenfullscreen = document.querySelector('.openfullscreen');
+const getclosefullscreen = document.querySelector('.closefullscreen');
+
 
 const videos = ['samplevideo1','samplevideo2'];
 
@@ -77,28 +84,81 @@ function previousvdo(){
 }
 
 
-const getdoce = document.documentElement;
+function updateprogress(){
+    // console.log('hey');
+
+    // currentTime,duration keywords come from video api
+    // console.log(getvideoscreen.currentTime);
+    // console.log(getvideoscreen.duration);
+
+    // console.log((getvideoscreen.currentTime / getvideoscreen.duration) * 100);
+
+    progress.value = (getvideoscreen.currentTime / getvideoscreen.duration) * 100;
+
+
+    let getmins = Math.floor(getvideoscreen.currentTime/60);
+    // console.log(getmins);
+
+    if(getmins < 10){
+        // getmins = '0' + getmins;
+        getmins = '0' + String(getmins);
+    }
+
+    let getsecs = Math.floor(getvideoscreen.currentTime%60);
+    // console.log(getsecs);
+
+    if(getsecs < 10){
+        // getsecs = '0' + getsecs;
+        getsecs = '0' + String(getsecs);
+    }
+
+
+    getdisplaytime.innerText = `${getmins}:${getsecs}`;
+
+
+}
+
+
+// const getdoce = document.documentElement;
 
 function openfullscreen(){
 
-    if(getdoce.requestFullscreen){
-        getdoce.requestFullscreen();
-    }else if(getdoce.webkitRequestFullscreen){
-        getdoce.webkitRequestFullscreen();
-    }else if(getdoce.msRequestFullscreen){
-        getdoce.msRequestFullscreen();
+    if(getcontainer.requestFullscreen){
+        getcontainer.requestFullscreen();
+    }else if(getcontainer.mozRequestFullscreen){
+        getcontainer.mozRequestFullscreen();
+    }else if(getcontainer.webkitRequestFullscreen){
+        getcontainer.webkitRequestFullscreen();
+    }else if(getcontainer.msRequestFullscreen){
+        getcontainer.msRequestFullscreen();
     }
+
+    getopenfullscreen.style.display = 'none';
+    getclosefullscreen.style.display = 'inline-block';
 }
 
 function closefullscreen(){
 
     if(document.exitFullscreen){
         document.exitFullscreen();
+    }else if(document.mozCancelFullscreen){
+        document.mozCancelFullscreen();
     }else if(document.webkitExitFullscreen){
         document.webkitExitFullscreen();
     }else if(document.msExitFullscreen){
         document.msExitFullscreen();
     }
+
+    getopenfullscreen.style.display = 'inline-block';
+    getclosefullscreen.style.display = 'none';
+}
+
+
+function setprogress(){
+    // console.log('hey');
+    // console.log((progress.value*getvideoscreen.duration)/100);
+
+    getvideoscreen.currentTime = (progress.value*getvideoscreen.duration)/100;
 }
 
 
@@ -107,6 +167,10 @@ playbtn.addEventListener('click',playpausevdo);
 nextbtn.addEventListener('click',nextvdo);
 prevbtn.addEventListener('click',previousvdo);
 
-getfullscreen.addEventListener('click',openfullscreen);
+getvideoscreen.addEventListener('timeupdate',updateprogress);
+progress.addEventListener('click',setprogress);
+
+getopenfullscreen.addEventListener('click',openfullscreen);
+getclosefullscreen.addEventListener('click',closefullscreen);
 
 
